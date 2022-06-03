97467e2f5ef5320c3b428b1cd500dbc6bd44f556
Charalampos Sartzis <61800771+CharalamposSartzis@users.noreply.github.com>
2022-06-03 19:22:01 +0000

    OK: COMMENTS student.h

--- .ccls-cache/@home@runner@2022-05-12CGPapmyCode/student.h
+++ .ccls-cache/@home@runner@2022-05-12CGPapmyCode/student.h
@@ -4,54 +4,52 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MAXSTRING 75         //Maximum 
-#define MINSTRING 2
+#define MAXSTRING 75            // Maximum string length. 
+#define MINSTRING 2             // Minimum string length. 
 
 
 
 
 
-typedef struct    //** Student
+typedef struct    // Student struct with the attributes of a student.
 {
-    char name[MAXSTRING + 1];      //**CHECK for input length. [+1]->'\0' end. name value: the address of the char array (of the 1st element).
+    char name[MAXSTRING + 1];      // (+1) is for the string's '\0' NULL-terminated character.
     int id;
 }Student;
 
 typedef struct nodeR* node;     // a type of pointer that holds the address of a node of the list (struct nodeR).
 typedef struct listR* list;     // a type of pointer that holds the address of the list of students (struct listR).
 
-//** CHECK values at the creation (if NULL).
-struct nodeR          //** TEST typedef.
+struct nodeR          // Node struct that contains the student's data/details (struct Student) and pointers to next and previous nodes.
 {
-    Student data;
+    Student data;        // the student's data/details (struct Student).
     node next;          // a pointer that holds the address of the next node of a list's node.
     node previous;      // a pointer that holds the address of the previous node of a list's node.
 };
 
-//** CHECK values at the creation (if NULL).
-struct listR
+struct listR       // List struct that represents a doubly-linked list of students.
 {
     node head;    // a pointer that holds the address of the first node of the list.
     node tail;    // a pointer that holds the address of the last node of the list.
 };
 
 
-int readInput(char* action);            //**
-bool empty_stdin();
-int getNumStudents();
-bool checkLen(char* string);
+int readInput(char* action);                // reads the user's input and returns the id given by the user for search/update/delete.
+bool empty_stdin();                        // clears the rest of the user's input, after reading an integer.
+int getNumStudents();                      // returns the number of students increased by one each time, in order to assign a unique id to each student.
+bool checkLen(char* string);               // checks the length of the strings.
 
-void printMenu();
-void print(Student st);
-void printstudents(list l);
+void printMenu();                            // prints the main menu with the choices.
+void print(Student st);                      // prints a specific student.
+void printstudents(list l);                  // prints all the students of the list.
 list load(char* filename);                  // retrieve the students' list from file.
 void save(char* filename, list l);          // save the students' list at file.
 int addStudent(Student st, list l);         // add new student to the list.
-Student* findStudent(int id, list l);       //** [return type ??] search for the student at the list, based on id.
+Student* findStudent(int id, list l);       // search for the student at the list, based on id.
 int deleteStudent(Student st, list l);      // delete the student from the list, based on st.id.
 int updateStudent(Student st, list l);      // update the student's details at the list, based on st.id.
 
-list createList();                        //**
-bool listIsEmpty(list l);                 //**
+list createList();                        // creates the doubly-linked list.
+bool listIsEmpty(list l);                 // checks if the list is empty.
 node createStNode(/*Student st*/);        // create a new node with the student's data.
 void insert(node stNode, list l);         // Insert a new student node at the end (tail) of the list.
